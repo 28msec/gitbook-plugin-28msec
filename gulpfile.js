d825f30c81437b81ab92feff3d666117b1da2e0c
William Candillon <wcandillon@gmail.com>
2015-02-27 16:23:49 +0100

    :new: First commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    var gulp = require('gulp');
+    var $ = require('gulp-load-plugins')();
+
+    gulp.task('jslint', function(){
+        return gulp.src(['gulpfile.js', 'index.js'])
+            .pipe($.jshint())
+            .pipe($.jshint.reporter())
+            .pipe($.jshint.reporter('fail'));
+    });
+
+    gulp.task('default', ['jslint']);
+}());
---
+++ index.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    module.exports = {
+        book: {
+            assets: ".",
+            css: [ "plugin.css" ]
+        },
+        hooks: {
+            "page": function(page) { // before html generation
+            }
+        }
+    };
+}());
