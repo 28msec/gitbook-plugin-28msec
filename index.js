6ddb4836d46b2b9db7d79a88a7c3014985f34d36
William Candillon <w@28.io>
2016-05-13 15:02:26 +0200

    :arrow_up:

--- index.js
+++ index.js
@@ -5,48 +5,8 @@
     var _ = require('lodash');
     var URL = require('url');
 
-    var api = require('./swagger-aggregated.json');
-
     var curlSnippet = (method, url) => {
         return `curl -X ${method} &quot;<a href="${url}" target="_blank">${url}</a>&quot;`;
-    };
-
-    var serializeJSValue = (param, value) => {
-        if(_.isArray(value)) {
-            return value.map(v => serializeJSValue(v));
-        } else {
-            try {
-                return JSON.parse(value);
-            } catch(e) {
-                return value;
-            }
-        }
-    };
-
-    var jsSnippet = (method, url) => {
-        var u = URL.parse(url, true);
-        var resource = api.paths[u.pathname.substring('/v1/_queries/public'.length)];
-        var op = resource[method.toLowerCase()];
-        return `API.${op.operationId}({
-        ${op.parameters
-            .filter(param => !param['x-exclude-from-bindings'])
-            .filter(param => u.query[param.name])
-            .map(param => `'${param.name}': ${JSON.stringify(serializeJSValue(param, u.query[param.name]))}`).join(',\n     ')}
-})`;
-    };
-
-    var csharpSnippet = (endpoint, method, url) => {
-        var u = URL.parse(url, true);
-        var resource = api.paths[u.pathname.substring('/v1/_queries/public'.length)];
-        var op = resource[method.toLowerCase()];
-        return `CellStore.Client.ApiClient client = new CellStore.Client.ApiClient(&quot;${endpoint}&quot;);
-CellStore.Api.DataApi api = new CellStore.Api.DataApi(client);
-
-api.${op.operationId[0].toUpperCase() + op.operationId.substring(1)}(${op.parameters
-            .filter(param => !param['x-exclude-from-bindings'])
-            .filter(param => u.query[param.name])
-            .map(param => `${param.name}: ${JSON.stringify(serializeJSValue(param, u.query[param.name]))}`).join(',\n     ')}
-)`;
     };
 
     var envs = {
